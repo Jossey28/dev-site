95280acd51e87d4adfa54ee2939f2a3c0231644c
Irene A <121013355+Jossey28@users.noreply.github.com>
2026-05-20 17:59:20 -0500

    Last working preview before switching to Leptos SSR w/ Axum

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use leptos::{IntoView, component, mount::mount_to_body};
-use leptos::prelude::*;
+use leptos::prelude::{view, ElementChild, ClassAttribute};
+use rand::{Rng, RngExt};
 
 fn main() {
     console_error_panic_hook::set_once();
@@ -9,28 +10,9 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-
     view! {
         <NavBar/>
-
         <Footer/>
-    }
-}
-
-// Below is an example of a doc component. These can be placed above the function, and above declared function parameters.
-/// This component shows progress toward a goal
-#[component]
-fn ProgressBar(
-    #[prop(default = 100)]
-    max: u16,
-    #[prop(into)]
-    progress: Signal<i32>
-) -> impl IntoView {
-    view! {
-        <progress
-            max=max
-            value=progress
-        />
     }
 }
 
@@ -52,8 +34,29 @@ fn NavBar() -> impl IntoView {
 
 #[component]
 fn Footer() -> impl IntoView {
+    let quotes = vec!["Accept everything just the way it is", "ts so chopped", "I'm employed bro 🫰", "i aint got none"];
+    let mut rng = rand::rng();
+    
+    let index: usize  = { // TODO! Fix Fix this horrible implementation
+        let num = rng.random::<u8>();
+        let i = num % quotes.len() as u8;
+        i as usize
+    };
+
+    let quote_opt = quotes.get(index);
+    let quote = match quote_opt {
+        Some(_) =>  { *quote_opt.unwrap() },
+        _ => "Hello there",
+    };
+
     view! {
-        <p>"Bye Bye"</p>
+        <footer>
+            <div>
+                <p>
+                    { format!("© 2026 Jossey28 | {:#?}™", quote) }
+                </p>
+            </div>
+        </footer>
     }
 }
 
