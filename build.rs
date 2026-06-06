6422d606e5f0f0add6b61b1938ca03e03d404405
Irene A <121013355+Jossey28@users.noreply.github.com>
2026-06-06 09:43:29 -0500

    Removed too-wide view bug. Working version tracker

--- build.rs
+++ build.rs
@@ -1,5 +1,17 @@
 use std::process::Command;
 
 fn main() { // Naive method to get current commit when running locally. TODO! Find alternatives 
-	let output = Command::new("git").args(["rev-parse", "HEAD"]).output().un;
+	let output = Command::new("git").args(["rev-parse", "HEAD"]).output();
+    match output {
+        Ok(out) => {
+            let commit = String::from_utf8(out.stdout).unwrap_or(String::from("Unable to covert command result to String"));
+            println!("cargo:rustc-env=GIT_COMMIT_SHA_RUST={}", commit);
+        }
+
+        Err(err) => {
+            println!("Unable to get command result; Error: {}", err);
+            return
+        }
+    }
+
 }
