9bd54d534e6de3a32e8462ef338ab724b84b2749
Jossey <121013355+Jossey28@users.noreply.github.com>
2026-07-15 11:44:37 -0500

    making links optional cause webring

--- src/app.rs
+++ src/app.rs
@@ -99,7 +99,7 @@ fn NavBar() -> impl IntoView {
 fn AboutMe() -> impl IntoView {
     let mat_88 = EightyEightData::new(
         "mat_does_dev-88x31.gif",
-        "https://matdoes.dev/",
+        Some("https://matdoes.dev/"),
         Some("Great collecter of 88x31s"),
     );
 
@@ -124,119 +124,104 @@ fn AboutMe() -> impl IntoView {
 }
 
 #[component]
-fn EightyEight(info: EightyEightData) -> impl IntoView {
-    view! {
-        <a href=info.link target="_blank">
-            <img
-                title=info.title.unwrap_or_else(|| "")
-                src=format!("/assets/88x31s/{}", info.image)
-                alt=format!("eighty eight by thirty one linking to {}", info.link)
-                width=88
-                height=31
-            />
-        </a>
-    }
-}
-
-#[component]
 fn Create88x31Row() -> impl IntoView {
     let mut eights: Vec<EightyEightData> = vec![
         EightyEightData::new(
             "opensource-88x31.gif",
-            "https://github.com/Jossey28/dev-site",
+            Some("https://github.com/Jossey28/dev-site"),
             None,
         ),
-        EightyEightData::new("powered_by_nixos-88x31.gif", "https://nixos.org/", None),
+        EightyEightData::new("powered_by_nixos-88x31.gif", Some("https://nixos.org/"), None),
         EightyEightData::new(
             "made_on_linux-88x31.gif",
-            "https://stallman-copypasta.github.io/",
+            Some("https://stallman-copypasta.github.io/"),
             None,
         ),
         EightyEightData::new(
             "rtr-88x31.gif",
-            "https://www.youtube.com/@rossmanngroup/",
+            Some("https://www.youtube.com/@rossmanngroup/"),
             None,
         ),
-        EightyEightData::new("rust-88x31.gif", "https://rust-lang.org/", None),
+        EightyEightData::new("rust-88x31.gif", Some("https://rust-lang.org/"), None),
         EightyEightData::new(
             "lumi-88x31.gif",
-            "https://lumiverse.dev/",
+            Some("https://lumiverse.dev/"),
             Some("Lumi!! qwq"),
         ),
         EightyEightData::new(
             "arch_btw-88x31.gif",
-            "https://archlinux.org/",
+            Some("https://archlinux.org/"),
             Some("I use arch btw"),
         ),
         EightyEightData::new(
             "xbox_live-88x31.gif",
-            "https://xenia-emulator.com/xbox-360-roms/",
+            Some("https://xenia-emulator.com/xbox-360-roms/"),
             None,
         ),
         EightyEightData::new(
             "xbox_live-88x31.gif",
-            "https://xenia-emulator.com/xbox-360-roms/",
+            Some("https://xenia-emulator.com/xbox-360-roms/"),
             None,
         ),
-        EightyEightData::new("hosted_on_a_pi-88x31.gif", "https://raspberrypi.com", None),
+        EightyEightData::new("hosted_on_a_pi-88x31.gif", Some("https://raspberrypi.com"), None),
         EightyEightData::new(
             "stop_sign-88x31.gif",
-            "https://matdoes.dev/buttons#d0f3c00fbd84b58dd3ab3cc353fc3ffd",
+            Some("https://matdoes.dev/buttons#d0f3c00fbd84b58dd3ab3cc353fc3ffd"),
             None,
         ),
         EightyEightData::new(
             "emulate_now-88x31.gif",
-            "https://matdoes.dev/buttons#d0f3c00fbd84b58dd3ab3cc353fc3ffd",
+            Some("https://matdoes.dev/buttons#d0f3c00fbd84b58dd3ab3cc353fc3ffd"),
             None,
         ),
         EightyEightData::new(
             "sleepy-88x31.gif",
-            "https://www.nhlbi.nih.gov/health/sleep/why-sleep-important",
+            Some("https://www.nhlbi.nih.gov/health/sleep/why-sleep-important"),
             None,
         ),
-        EightyEightData::new("hacker_powered-88x31.gif", "https://iateched.org/", None),
+        EightyEightData::new("hacker_powered-88x31.gif", Some("https://iateched.org/"), None),
         EightyEightData::new(
             "cookie_free-88x31.gif",
-            "https://improvado.io/blog/what-is-tracking-pixel",
+            Some("https://improvado.io/blog/what-is-tracking-pixel"),
             Some("It gets to a point"),
         ),
-        EightyEightData::new("archive-88x31.gif", "https://archive.org/donate", None),
+        EightyEightData::new("archive-88x31.gif",Some("https://archive.org/donate"), None),
         EightyEightData::new(
             "copy_that_floppy-88x31.gif",
-            "https://archive.org/donate",
+           Some("https://archive.org/donate"),
             None,
         ),
-        EightyEightData::new("neocities-88x31.gif", "https://neocities.org/", None),
+        EightyEightData::new("neocities-88x31.gif", Some("https://neocities.org/"), None),
         EightyEightData::new(
             "midi_files-88x31.gif",
-            "https://www.nccih.nih.gov/health/music-and-health-what-you-need-to-know",
+            Some("https://www.nccih.nih.gov/health/music-and-health-what-you-need-to-know"),
             None,
         ),
-        EightyEightData::new("dr_pepper-88x31.gif", "https://drpepper.com/en", None),
+        EightyEightData::new("dr_pepper-88x31.gif", Some("https://drpepper.com/en"), None),
         EightyEightData::new(
             "im_no_diva-88x31.gif",
-            "https://dev-site.homecamp.biz",
+            Some("https://dev-site.homecamp.biz"),
             None,
         ),
         EightyEightData::new(
             "steam_powered-88x31.gif",
-            "https://web.archive.org/web/20041115052057/http://steampowered.com/",
+            Some("https://web.archive.org/web/20041115052057/http://steampowered.com/"),
             None,
         ),
         EightyEightData::new(
             "kitrinos-88x31.gif",
-            "https://kitrinos.neocities.org/",
+            Some("https://kitrinos.neocities.org/"),
             None,
         ),
         EightyEightData::new(
             "cool_graphics-88x31.gif",
-            "https://codepen.io/trending",
+            Some("https://codepen.io/trending"),
             None,
         ),
-        EightyEightData::new("hellnet-88x31.gif", "https://hellnet.work/", None),
+        EightyEightData::new("hellnet-88x31.gif", Some("https://hellnet.work/"), None),
         EightyEightData::new(
             "wikipedia-88x31.gif",
-            "https://en.wikipedia.org/wiki/Main_Page",
+            Some("https://en.wikipedia.org/wiki/Main_Page"),
             None,
         ),
     ];
@@ -264,16 +249,31 @@ fn Create88x31Row() -> impl IntoView {
     }
 }
 
+#[component]
+fn EightyEight(info: EightyEightData) -> impl IntoView {
+    view! {
+        <a href=info.link target="_blank">
+            <img
+                title=info.title.unwrap_or_else(|| "")
+                src=format!("/assets/88x31s/{}", info.image)
+                alt=format!("eighty eight by thirty one linking to {}", info.link.unwrap_or_else(|| "nowhere :)"))
+                width=88
+                height=31
+            />
+        </a>
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 struct EightyEightData {
     image: &'static str,
-    link: &'static str,
+    link: Option<&'static str>,
 
     title: Option<&'static str>,
 }
 
 impl EightyEightData {
-    fn new(image: &'static str, link: &'static str, title: Option<&'static str>) -> Self {
+    fn new(image: &'static str, link: Option<&'static str>, title: Option<&'static str>) -> Self {
         Self { image, link, title }
     }
 }
@@ -302,6 +302,7 @@ fn Projects() -> impl IntoView {
 
 #[component]
 pub fn GithubActivity() -> impl IntoView {}
+
 #[component]
 fn Footer() -> impl IntoView {
     let quotes = vec![
@@ -339,7 +340,7 @@ fn Footer() -> impl IntoView {
 
     let next = EightyEightData::new(
         "continue_the_ring-88x31.gif",
-        "https://crizer.dingusblingus.cc/",
+        None,
         None,
     );
 
@@ -362,7 +363,7 @@ fn Footer() -> impl IntoView {
             </small>
 
             <span id="ring-next" onclick="gleebusOpen(1)">
-                "Next"
+                <EightyEight info=next />
             </span>
         </footer>
     }
