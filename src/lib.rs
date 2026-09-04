e3178302d70af4bacf82e46f53060319c49a948a
Jossey28 <121013355+Jossey28@users.noreply.github.com>
2026-09-04 10:38:52 -0500

    Added better error handling during init; Contemplating hydrating islands;

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,5 @@ pub fn hydrate() {
     use crate::app::*;
     console_error_panic_hook::set_once();
     leptos::mount::hydrate_body(App);
+    // leptos::mount::hydrate_islands();
 }
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ async fn main() {
     use leptos::prelude::*;
     use leptos_axum::{LeptosRoutes, generate_route_list};
 
-    let conf = get_configuration(None).unwrap();
-    let addr = "0.0.0.0:8080";
+    #[allow(clippy::expect_used)]
+    let conf = get_configuration("./Cargo.toml".into()).expect("Failed to pull configuration");
+    let addr = conf.leptos_options.site_addr;
     let leptos_options = conf.leptos_options;
     let routes = generate_route_list(App);
 
@@ -25,19 +26,28 @@ async fn main() {
         use tower_livereload::LiveReloadLayer;
         let app = app.clone().layer(LiveReloadLayer::new());
         log!("LIVE RELOAD ON ; listening on http://{}", &addr);
-        let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+        #[allow(clippy::expect_used)]
+        let listener = tokio::net::TcpListener::bind(&addr)
+            .await
+            .expect("Unable to bind to port");
+        #[allow(clippy::expect_used)]
+        // Since theres no point in continuing if either of these fail
         axum::serve(listener, app.into_make_service())
             .await
-            .unwrap();
+            .expect("Failed to serve app");
     }
 
     #[cfg(not(debug_assertions))]
     {
         log!("listening on http://{}", &addr);
-        let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+        #[allow(clippy::expect_used)]
+        let listener = tokio::net::TcpListener::bind(&addr)
+            .await
+            .expect("Unable to bind to port");
+        #[allow(clippy::expect_used)]
         axum::serve(listener, app.into_make_service())
             .await
-            .unwrap();
+            .expect("Failed to serve app");
     }
 }
 
